68a7f9f2af7369c3bc9d30f4a2e4cfc27d306b6f
schoonmt@gmail.com <schoonmt@gmail.com>
2018-05-04 00:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidayTo(name) {
-  return `Happy holidays, ${name}!`
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
